8ef68e1914e145bb2ea250450b5f98107bf7ed01
Tiba Folzen <tiba.folzen@gmail.com>
2017-01-17 03:03:38 +1000

    add some random positioned walls collision detection and preventing of walking into texture

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,61 @@
 #include <random>
 #include <time.h>
 
+
+sf::Vector2f get_sprite_size(sf::Sprite &s1) {
+    sf::Vector2f result;
+    sf::IntRect texture_size = s1.getTextureRect();
+    sf::Vector2f scale = s1.getScale();
+    result.x = texture_size.width * scale.x;
+    result.y = texture_size.height * scale.y;
+    return result;
+}
+
+
+bool check_collision(sf::Sprite &s1, sf::Sprite &s2) {
+    sf::Vector2f s1_size = get_sprite_size(s1);
+    sf::Vector2f s2_size = get_sprite_size(s2);
+
+    sf::Vector2f s1_pos = s1.getPosition();
+    sf::Vector2f s2_pos = s2.getPosition();
+
+    return (
+       ((s1_pos.x + s1_size.x / 2) >= (s2_pos.x - s2_size.x / 2))
+      &((s1_pos.x - s1_size.x / 2) <= (s2_pos.x + s2_size.x / 2))
+      &((s1_pos.y + s1_size.y / 2) >= (s2_pos.y - s2_size.y / 2))
+      &((s1_pos.y - s1_size.y / 2) <= (s2_pos.y + s2_size.y / 2))
+    );
+}
+
 int main(int argc, char * argv[]) {
     srand(std::time(0));
 
+    //create window
     sf::RenderWindow window(sf::VideoMode(640, 480), "Brand cool title :D:D:D");
-    sf::Texture test_arrow_texture;
+
+    //declare textures
+    sf::Texture test_arrow_texture, wall_texture;
+
+    //init textures
     test_arrow_texture.loadFromFile("asset/img/test_arrow.png");
+    wall_texture.loadFromFile("asset/img/wall.png");
+
+    //create sprites
     sf::Sprite myarrow(test_arrow_texture);
+    myarrow.setOrigin(0,0);
+
+    int wall_count = 5;
+    sf::Sprite mywalls[wall_count];
+
+    for (int i = 0; i < wall_count; i++) {
+        mywalls[i].setTexture(wall_texture);
+        mywalls[i].setOrigin(0,0);
+
+        int x,y;
+        x = std::rand() % 600;
+        y = std::rand() % 400;
+        mywalls[i].setPosition(x, y);
+    }
 
     while (window.isOpen()) {
         sf::Event event;
@@ -28,25 +76,41 @@ int main(int argc, char * argv[]) {
         sf::Color bg_color(r,g,b);
 
         //logic
+        int myarrow_xoffset, myarrow_yoffset;
+        myarrow_xoffset = 0;
+        myarrow_yoffset = 0;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            myarrow.move(0, -1);
+            myarrow_yoffset--;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            myarrow.move(-1, 0);
+            myarrow_xoffset--;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            myarrow.move(0, 1);
+            myarrow_yoffset++;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            myarrow.move(1, 0);
+            myarrow_xoffset++;
         }
+        myarrow.move(myarrow_xoffset, myarrow_yoffset);
 
+        bool collide = false;
+        for (int i = 0; i < wall_count; i++) {
+            if (check_collision(myarrow, mywalls[i])) {
+                collide = true;
+            }
+        }
+        if (collide) {
+            myarrow.move(-myarrow_xoffset, -myarrow_yoffset);
+        }
 
         //render
         window.clear(bg_color);
+        for (int i = 0; i < wall_count; i++) {
+            window.draw(mywalls[i]);
+        }
         window.draw(myarrow);
         window.display();
     }
