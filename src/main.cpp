cb07a70a6e8b76c854a9736690c514fee965a352
Tiba Folzen <tiba.folzen@gmail.com>
2017-01-16 22:19:21 +1000

    add window with title add background color add sprite add window close handling 	modified:   CMakeLists.txt 	new file:   CMakeLists.txt.user 	new file:   asset/img/test_arrow.png 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
+#include <SFML/Graphics.hpp>
+
 int main(int argc, char * argv[]) {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Brand cool title :D:D:D");
+    sf::Texture test_arrow_texture;
+    test_arrow_texture.loadFromFile("asset/img/test_arrow.png");
+    sf::Sprite myarrow(test_arrow_texture);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Green);
+        window.draw(myarrow);
+        window.display();
+    }
     return 0;
 }
