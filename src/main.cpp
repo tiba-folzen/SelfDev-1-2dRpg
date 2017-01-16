db4b42336d7fdcb1ab29168a7bc039d20a6790e4
Tiba Folzen <tiba.folzen@gmail.com>
2017-01-16 22:56:42 +1000

    add random functions for later use cmake: added -std=c++17 to use latest&gratest c++ (actually just for random)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include <random>
+#include <time.h>
 
 int main(int argc, char * argv[]) {
+    srand(std::time(0));
+
     sf::RenderWindow window(sf::VideoMode(640, 480), "Brand cool title :D:D:D");
     sf::Texture test_arrow_texture;
     test_arrow_texture.loadFromFile("asset/img/test_arrow.png");
@@ -14,7 +18,16 @@ int main(int argc, char * argv[]) {
             }
         }
 
-        window.clear(sf::Color::Green);
+        int r,g,b;
+        r = 255; g = 255; b = 255;
+        /*
+        r = std::rand() % 256;
+        g = std::rand() % 256;
+        b = std::rand() % 256;
+        */
+        sf::Color bg_color(r,g,b);
+
+        window.clear(bg_color);
         window.draw(myarrow);
         window.display();
     }
